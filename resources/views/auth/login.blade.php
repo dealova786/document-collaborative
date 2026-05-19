699bee1d24a76fb155f3f33e99c9cb7fb28f7040
Dea Lova Asmara <dea.240180084@mhs.unimal.ac.id>
2026-05-19 21:02:40 +0700

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,14 +32,25 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-end mt-4 gap-3">
+            <div class="flex gap-3">
+
+            <a
+                href="{{ route('register') }}"
+                class="underline text-sm text-gray-600 hover:text-gray-900"
+            >
+                Register
+            </a>
+
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a 
+                    class="underline text-sm text-gray-600 hover:text-gray-900" 
+                    href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button>
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
